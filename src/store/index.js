f74a2502c6d25cbb1e9294ee8fa87ae6543daac9
Eduardo Razo <desarrollo@gatewayfabrication.com.mx>
2022-12-19 10:54:20 -0600

    Instalación y boilerplate de Vuex 4/19

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
+//funcion para crear un sistema de almacenaje de datos
 import { createStore } from 'vuex'
 
+//Opciones de configuración del sistema de datos 
 const store = createStore({
   
 })
 
+//Exportarlo para que vuejs importarlo y utilizarlo
 export default store
