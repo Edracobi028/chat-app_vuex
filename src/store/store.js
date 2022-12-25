2bf0182355bc27177ba60dd3b40597f2d2ab461b
Eduardo Razo <esse_rios_e@hotmail.com>
2022-12-24 20:03:43 -0600

    Normalización 14/19

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,59 @@ import { reactive } from 'vue';
 
 //recibe objetos para representar de forma reactiva
 const store = reactive({
-    username: 'Edracobi028',
-    //para actualizar recibe el valor accede al actual y asigna uno nuevo
-    updateUsername(username){
-        this.username = username;
-    }
+    /* persona que esta usando la app */
+    profile: {
+        username: ''
+    },
+    contacts: [
+        {
+            id: 1,
+            username: 'User 1',
+        },
+        {
+            id: 2,
+            username: 'User 2',
+        }
+    ],
+    messages: [
+        {
+            id: 1,
+            msg: 'Hola:D',
+            author: 1
+
+        },
+        {
+            id: 2,
+            msg: 'keondas!!',
+            author: 2
+        },
+        {
+            id: 3,
+            msg: 'Hola:D',
+            author: 1
+
+        },
+        {
+            id: 4,
+            msg: 'keondas!!',
+            author: 2
+        }
+
+    ],
+    /* Lista de Conversasiones */
+    channels: [
+        /* conversacion */
+        {
+            id: 1,
+            name: 'Canal 1',
+            messages: [ 1,2 ]
+        },
+        {
+            id: 2,
+            name: 'Canal 2',
+            messages: [ 3,4 ]
+        }
+    ]
 })
 
 
